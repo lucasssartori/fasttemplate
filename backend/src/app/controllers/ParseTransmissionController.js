f0e15b3306b035482bb83513fa42a4e835faa539
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2020-05-21 18:09:11 -0300

    Parse Transmission

--- backend/src/app/controllers/ParseTransmissionController.js
+++ backend/src/app/controllers/ParseTransmissionController.js
@@ -1,6 +1,4 @@
 import fs from 'fs';
-import { NONAME } from 'dns';
-import { IGNORE } from 'sequelize/types/lib/index-hints';
 
 class ParseTransmissionController {
   /*
@@ -16,78 +14,144 @@ class ParseTransmissionController {
   async store(req, res) {
     const { name } = req.body;
 
-    fs.readFile(
-      `Z:\\COPERNIC_PASTAS_INDICE\\01. Fontes Dimensions R1\\PRODUCAO_JCL\\${name}.txt`,
-      'utf8',
-      (err, data) => {
-        try {
-          const lines = data.split('\r\n');
+    const sigla_ambientes = ['U', 'H', 'N', 'R'];
+    const transmissions = [];
 
-          const transmission = {
-            tech_in: '',
-            tech_for: '',
-            server_in: '',
-            server_for: '',
-            directory_in: '',
-            directory_for: '',
-            user_in: '',
-            user_for: '',
-            mask_archive_in: '',
-            mask_archive_for: '',
-            size_register_in: '',
-            size_register_for: '',
-            node_in: '',
-            node_for: '',
-            application_in: '',
-            application_for: '',
-            solution_agent_in: '',
-            solution_agent_for: '',
-            process_in: '',
-            process_for: '',
-          };
+    function Parse(job_name) {
+      fs.readFile(
+        `Z:\\COPERNIC_PASTAS_INDICE\\01. Fontes Dimensions R1\\PRODUCAO_JCL\\${job_name}.txt`,
+        'utf8',
+        (err, data) => {
+          try {
+            const lines = data.split('\r\n');
 
-          const transmission_pelican = false;
+            const transmission = {
+              tech_in: 'N/A',
+              tech_for: 'N/A',
+              server_in: 'N/A',
+              server_for: 'N/A',
+              directory_in: 'N/A',
+              directory_for: 'N/A',
+              user_in: 'N/A',
+              user_for: 'N/A',
+              mask_archive_in: 'N/A',
+              mask_archive_for: 'N/A',
+              size_register_in: 'N/A',
+              size_register_for: 'N/A',
+              node_in: 'N/A',
+              node_for: 'N/A',
+              application_in: 'N/A',
+              application_for: 'N/A',
+              solution_agent_in: 'N/A',
+              solution_agent_for: 'N/A',
+              process_in: 'N/A',
+              process_for: 'N/A',
+            };
 
-          for (let i = 0; i < lines.length; i++) {
-            let trim_line = lines[i].replace(/\s{2,}/g, ' ');
-            trim_line = trim_line.trim();
+            let transmission_pelican = false;
+            let transmission_conect = false;
+            let separate = '';
 
-            if (trim_line.search('EXEC PELREQ') !== -1) {
-              transmission.tech_in = 'PELICAN';
-              transmission.tech_for = 'PELICAN';
-              transmission_pelican = true;
-            }
-            if (transmission_pelican) {
-              const words = lines[i].split('=');
+            lines.forEach((line) => {
+              let trim_line = line.replace(/\s{2,}/g, ' ');
+              trim_line = trim_line.trim();
 
-              console.log(words[1]);
-
-              switch (words[0]) {
-                case 'DEST':
-                  //                  transmission.server_for = words[1];
-                  transmission.server_in = 'MG';
-                  break;
-                case 'N':
-                  //                  transmission.application_in = words[1];
-                  console.log('teste');
-                  break;
-                default:
-                  console.log('nada');
+              if (trim_line.search('EXEC PELREQ') !== -1) {
+                transmission.tech_in = 'PELICAN';
+                transmission.tech_for = 'PELICAN';
+                transmission_pelican = true;
               }
-            }
+
+              if (trim_line.search('EXEC CONECT') !== -1) {
+                transmission.tech_in = 'CONECT';
+                transmission.tech_for = 'CONECT';
+                transmission_conect = true;
+              }
+
+              if (transmission_pelican) {
+                const [atribute, value] = trim_line.split('=');
+                switch (atribute) {
+                  case 'LOCAL':
+                    transmission.server_in = value;
+                    break;
+                  case 'DEST':
+                    transmission.server_for = value;
+                    break;
+                  case 'N':
+                    transmission.application_in = value;
+                    break;
+                  case 'DSNORIG':
+                    transmission.mask_archive_in = value;
+                    break;
+                  case 'MSG1':
+                    transmission.mask_archive_for = value;
+                    break;
+                  case 'MSG2':
+                    transmission.directory_for = value;
+                    break;
+                  case 'COMP':
+                    transmission_pelican = false;
+                    transmissions.push(transmission);
+                    break;
+                  default:
+                }
+              }
+
+              if (transmission_conect) {
+                const [atribute, value] = trim_line.split('=');
+                switch (atribute) {
+                  case 'SIGNON USERID':
+                    separate = value.indexOf(',');
+                    transmission.user_in = value.substr(1, separate);
+                    break;
+                  case 'SUBMIT PROC':
+                    transmission.application_in = value.substr(
+                      0,
+                      value.length - 2
+                    );
+                    break;
+                  case '&ORIGEM':
+                    transmission.server_in = value.substr(0, value.length - 2);
+                    break;
+                  case '&DESTINO':
+                    transmission.server_for = value.substr(0, value.length - 2);
+                    break;
+                  case '&ARQORIG':
+                    transmission.mask_archive_in = value.substr(
+                      0,
+                      value.length - 2
+                    );
+                    break;
+                  case '&ARQDEST':
+                    transmission.mask_archive_for = value;
+                    break;
+                  case 'SIGNOFF':
+                    transmission_conect = false;
+                    transmissions.push(transmission);
+                    console.log(transmissions);
+                    console.log('aki 3');
+                    break;
+                  default:
+                }
+              }
+            });
+          } catch (erro) {
+            console.log(err);
           }
-          console.log('sair');
-          return res.json({
-            transmission,
-          });
-        } catch (erro) {
-          return res.json({
-            err,
-            erro,
-          });
         }
-      }
-    );
+      );
+    }
+
+    if (name.substr(0, 1) === '#') {
+      console.log('aki 1');
+      sigla_ambientes.forEach((sigla) => Parse(name.replace('#', sigla)));
+    } else {
+      console.log('aki 2');
+      Parse(name);
+      console.log(transmissions);
+    }
+
+    return res.json(transmissions);
   }
 }
 
